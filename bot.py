344da6e4f2ca6cf0ffd266d2dd802b4dc9c94676
Mohamad-Al-Telawe <100032861+Mohamad-Al-Telawe@users.noreply.github.com>
2025-06-15 18:04:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-import os
-
-TOKEN = os.getenv("7908716419:AAGpFWjBLRDMDQtzOH-oa2vmB2DBQhFziBo")
+# ضع التوكن مباشرة كسلسلة نصية
+TOKEN = "7908716419:AAGpFWjBLRDMDQtzOH-oa2vmB2DBQhFziBo"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("👋 أهلاً بك في بوت البايثون!")
@@ -11,4 +10,5 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 if __name__ == '__main__':
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
+    print("✅ البوت يعمل الآن ...")
     app.run_polling()
